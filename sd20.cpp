4842797e0040066af6051076f561a5e170016e36
seattlenomnom <mark.crapser@gmail.com>
2025-04-02 00:13:22 -0700

    Some idea need working out, p. 80

--- sd20.cpp
+++ sd20.cpp
@@ -7,8 +7,12 @@
  *Tue 01 Apr 2025 11:50:09 PM PDT
  * 20.3 Functions parameters
  * 20.4 Passing Arguments
+ *    This contains some differences from C. A little more study here.
  * 20.5 Function Overloads
  *
+ *
+ * Next:
+ * study p. 80, various ways of passing arguements in C++ vs. C.
 */
 
 
